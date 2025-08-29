452692948b1fb6c5fe155f864a783c1d045d219e
kartik11m <kartikmaheshwari698@gmail.com>
2025-08-30 01:54:13 +0530

    Created the middleware to protect admin routes from user

---
+++ middleware/auth.js
@@ -0,0 +1,21 @@
+import { clerkClient } from "@clerk/express";
+
+export const protectAdmin = async(req,res,next)=>{
+    try{
+        const {userId} = req.auth();
+
+        const user = await clerkClient.users.getUser(userId);
+
+        if(user.privateMetadata.role !== 'admin'){
+            return res.json({success: false , message:"not authorized"})
+        }
+
+        next();
+        // for controller function
+    }catch(error){
+        return res.json({success: false , message:error.message})
+    }
+}
+
+// This function is created so that user cannot get access to the admin things like adding shows
+// means protecting the admin routes from the user
