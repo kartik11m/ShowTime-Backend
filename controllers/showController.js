a69b6e45cf90253e564a5526b811935ca2601541
kartik11m <kartikmaheshwari698@gmail.com>
2025-08-30 00:39:36 +0530

    fixing the error due to autotype

--- controllers/showController.js
+++ controllers/showController.js
@@ -3,7 +3,7 @@ import axios from "axios"
 export const getNowPlayingMovies = async (req,res) =>{
     try{
         const {data} = await axios.get('https://api.themoviedb.org/3/movie/now_playing',{
-            headers: {Authorization: `Bearer ${ProcessingInstruction.env.TMDB_API_KEY}`}
+            headers: {Authorization: `Bearer ${process.env.TMDB_API_KEY}`}
         });
 
         const movies = data.results;
