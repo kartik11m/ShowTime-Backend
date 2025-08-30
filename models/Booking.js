bd04a3e2b631d094890cc9b600053272945f333c
kartik11m <kartikmaheshwari698@gmail.com>
2025-08-31 01:34:26 +0530

    changing small letter to Capital letter to fix an error

--- models/Booking.js
+++ models/Booking.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const bookingSchema = new mongoose.Schema({
     user: {type: String, required: true,ref:'User'},
     show: {type: String, required: true,ref:'Show'},
-    amount: {type: number, required: true},
+    amount: {type: Number, required: true},
     bookedSeats: {type: Array, required: true},
     isPaid: {type: Boolean, default:false},
     paymentLink: {type: String},
