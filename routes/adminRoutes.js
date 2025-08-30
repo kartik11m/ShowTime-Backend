9da3ee68f1561347561026766d210af15b9ee18b
kartik11m <kartikmaheshwari698@gmail.com>
2025-08-31 02:48:42 +0530

    minor fixes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { protectAdmin } from "../middleware/auth";
+import { protectAdmin } from "../middleware/auth.js";
 import { getAllBookings, getAllShows, getDashboardData, isAdmin } from "../controllers/adminController.js";
 
 const adminRouter = express.Router();
