4bab65ed1cd92c682cabc8272180d2d068aea691
kartik11m <kartikmaheshwari698@gmail.com>
2025-09-02 01:14:21 +0530

    Added an if statement for authentication of user as admin

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,6 +3,9 @@ import { clerkClient } from "@clerk/express";
 export const protectAdmin = async(req,res,next)=>{
     try{
         const {userId} = req.auth();
+        if (!userId) {
+        return res.json({success: false, message: "User not authenticated"});
+        }
 
         const user = await clerkClient.users.getUser(userId);
 
