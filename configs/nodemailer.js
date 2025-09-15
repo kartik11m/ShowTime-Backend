c55c1f18f749a245425d95c54805e8f184b74498
kartik11m <kartikmaheshwari698@gmail.com>
2025-09-16 03:26:19 +0530

    Correct casing to nodemailer.js

---
+++ configs/nodemailer.js
@@ -0,0 +1,27 @@
+import nodemailer from 'nodemailer';
+import React from 'react';
+
+// Changed the file name from nodeMailer to nodemailer
+
+// Create a transporter for SMTP
+const transporter = nodemailer.createTransport({
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false, // upgrade later with STARTTLS
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+
+const sendEmail = async({to,subject,body}) =>{
+    const response = await transporter.sendMail({
+        from: process.env.SENDER_EMAIL,
+        to,
+        subject,
+        html: body,
+    })
+    return response;
+}
+
+export default sendEmail;
